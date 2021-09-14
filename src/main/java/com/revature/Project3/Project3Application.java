4ed26edb612e5933210741bdc6536d4956d7f630
aruna <arunajava567@gmail.com>
2021-09-14 19:12:33 +0530

    first commit

---
+++ src/main/java/com/revature/Project3/Project3Application.java
@@ -0,0 +1,20 @@
+package com.revature.Project3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Project3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3Application.class, args);
+	}
+	@RequestMapping("/project3")
+	public String hello() {
+		return "Hello";
+	}
+
+}
